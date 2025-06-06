b016f4312efa78ced02dfc23bfcf93899c299c55
KYALOJACK <jackkyalo21@gmail.com>
2025-06-06 16:44:44 +0300

    feat: initialize alx-project-2 with Next.js and TypeScript setup
    - Added package.json with dependencies for React, Next.js, and TypeScript.
    - Created global styles in globals.css using Tailwind CSS.
    - Implemented custom _app.tsx and _document.tsx for Next.js.
    - Developed pages: index.tsx, about.tsx, home.tsx, posts.tsx, and users.tsx.
    - Added API route hello.ts for basic API response.
    - Created reusable components: Header, Button, Card, PostModal, PostCard, and UserCard.
    - Configured PostCSS with Tailwind CSS.
    - Included SVG assets in the public directory.
    - Set up TypeScript configuration in tsconfig.json.

---
+++ alx-project-0x02/pages/users.tsx
@@ -0,0 +1,32 @@
+// pages/users.tsx
+import { GetStaticProps } from 'next';
+import { UserProps } from '../interfaces';
+import UserCard from '@/components/common/UserCard';
+import Header from '@/components/layout/Header';
+
+interface UsersPageProps {
+  users: UserProps[];
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users: UserProps[] = await res.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
+};
+
+export default function UsersPage({ users }: UsersPageProps) {
+  return (
+    <main className="p-8 bg-gray-100 min-h-screen">
+      <Header />
+      <h1 className="text-3xl font-bold mb-6">Users</h1>
+      {users.map((user) => (
+        <UserCard key={user.id} {...user} />
+      ))}
+    </main>
+  );
+}
